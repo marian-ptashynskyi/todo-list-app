5394b4b2d1826bffcc2a0c3ed6a076e401d7fb60
marian-ptashynskyi <marian-ptashynskyi@users.noreply.github.com>
2019-05-31 11:42:48 +0300

    More refactoring

--- baseURL.js
+++ baseURL.js
@@ -1,1 +1,3 @@
-export default BASE_URL = 'http://192.168.0.105:5000/api/todo';
+//export default BASE_URL = 'http://192.168.0.105:5000/api/todo';
+
+export default BASE_URL = 'http://192.168.103.121:5000/api/todo';
--- reducers/index.js
+++ reducers/index.js
@@ -1,11 +1,50 @@
 import { combineReducers } from 'redux';
 
-import { hasErrored, isFetching } from './fetchTodosReducer';
-import { text, todos } from './todosReducer';
+import * as ActionTypes from '../actions/actionTypes';
 
-export default combineReducers({
-    hasErrored,
-    isFetching,
-    text,
-    todos
-});
+const initialState = {
+    hasErrored: false,
+    isFetching: false,
+    text: '',
+    todos: []
+}
+
+export function rootReducer(state = initialState, action) {
+    switch (action.type) {
+        case ActionTypes.ADD_TASK:
+            return {
+                ...state,
+                text: '',
+                todos: [...state.todos, action.task]
+            }
+        case ActionTypes.CHANGE_TEXT:
+            return {
+                ...state,
+                text: action.text,
+            }
+        case ActionTypes.DELETE_TASK: {
+            let filtered = state.todos.filter(task => task.id !== action.id);
+            return {
+                ...state,
+                todos: filtered,
+            }
+        }
+        case ActionTypes.FETCH_TODOS_FAILURE:
+            return {
+                ...state,
+                hasErrored: action.hasErrored,
+            }
+        case ActionTypes.FETCH_TODOS_REQUEST:
+            return {
+                ...state,
+                isFetching: action.isFetching,
+            }
+        case ActionTypes.FETCH_TODOS_SUCCESS:
+            return {
+                ...state,
+                todos: action.todos,
+            }
+        default:
+            return state;
+    }
+}
